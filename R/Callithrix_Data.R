ed8b995dfba563752821d2c80d1a33eda76e73e3
ctross <ctross@ucdavis.edu>
2023-09-11 14:23:13 +0200

    Update Callithrix_Data.R

--- R/Callithrix_Data.R
+++ R/Callithrix_Data.R
@@ -1,12 +1,13 @@
-#' Data on Callithrix agression
+#' Data on Callithrix aggression
 #'
-#' Foraging networks and social tolerance in a cooperatively breeding primate (Callithrix jacchus) 
+#' Foraging networks and social tolerance in a cooperatively breeding primate (Callithrix jacchus). See the reference to the original study below for full methodological information. 
 #'
 #' @docType data
 #'
 #' @usage data(Callithrix_Data)
 #'
-#' @format An object of class \code{"list"}.
+#' @format An object of class \code{"list"}. This data object contains a subset of data from the original publication. The first sub-list contains data from the C-, or low food, condition.
+#' The second sub-list contains data from the S++, or high food, condition. Both sub-lists contain data on dyadic aggression, normalized directed rank difference, opportunity to be observed, and sex. 
 #'
 #' @keywords datasets
 #'
@@ -17,5 +18,3 @@
 #' @examples
 #' data(Callithrix_Data)
 "Callithrix_Data"
-
-
