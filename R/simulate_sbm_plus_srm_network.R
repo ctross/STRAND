e68cdf8f94f131ef6d25434a370286c51e2ab144
Cody T. Ross <ross.cody.taylor@gmail.com>
2024-10-04 10:53:32 +0200

    Add files via upload

--- R/simulate_sbm_plus_srm_network.R
+++ R/simulate_sbm_plus_srm_network.R
@@ -3,37 +3,22 @@
 #' This is a function to simulate single layer network data with a stochastic block structure, sender-receiver effects, and dyadic reciprocity. This function
 #' is essentially the union of a social relations model and a stochastic block model.
 #'
-#' @param 
-#' N_id Number of individuals.
-#' @param 
-#' B List of matrices that hold intercept and offset terms. Log-odds. The first matrix should be  1 x 1 with the value being the intercept term.
-#' @param 
-#' V Number of blocking variables in B.
-#' @param 
-#' groups Dataframe of the block IDs of each individual for each variable in B.
-#' @param 
-#' sr_mu Mean A vector for sender and receivier random effects. In most cases, this should be c(0,0).
-#' @param 
-#' dr_mu Mean A vector for dyadic random effects. In most cases, this should be c(0,0).
-#' @param 
-#' sr_sigma A standard deviation vector for sender and receivier random effects. The first element controls node-level variation in out-degree, the second in in-degree.
-#' @param 
-#' dr_sigma Standard deviation for dyadic random effects.
-#' @param 
-#' sr_rho Correlation of sender-receiver effects (i.e., generalized reciprocity).
-#' @param 
-#' dr_rho Correlation of dyad effects (i.e., dyadic reciprocity).
-#' @param 
-#' mode Outcome mode: can be "bernoulli", "poisson", or "binomial".
-#' @param 
-#' individual_predictors An N_id by N_individual_parameters matrix of covariates.
-#' @param 
-#' dyadic_predictors An N_id by N_id by N_dyadic_parameters array of covariates.
-#' @param 
-#' individual_effects A 2 by N_individual_parameters matrix of slopes. The first row gives effects of focal characteristics (on out-degree). 
+#' @param N_id Number of individuals.
+#' @param B List of matrices that hold intercept and offset terms. Log-odds. The first matrix should be  1 x 1 with the value being the intercept term.
+#' @param V Number of blocking variables in B.
+#' @param groups Dataframe of the block IDs of each individual for each variable in B.
+#' @param sr_mu Mean A vector for sender and receivier random effects. In most cases, this should be c(0,0).
+#' @param dr_mu Mean A vector for dyadic random effects. In most cases, this should be c(0,0).
+#' @param sr_sigma A standard deviation vector for sender and receivier random effects. The first element controls node-level variation in out-degree, the second in in-degree.
+#' @param dr_sigma Standard deviation for dyadic random effects.
+#' @param sr_rho Correlation of sender-receiver effects (i.e., generalized reciprocity).
+#' @param dr_rho Correlation of dyad effects (i.e., dyadic reciprocity).
+#' @param mode Outcome mode: can be "bernoulli", "poisson", or "binomial".
+#' @param individual_predictors An N_id by N_individual_parameters matrix of covariates.
+#' @param dyadic_predictors An N_id by N_id by N_dyadic_parameters array of covariates.
+#' @param individual_effects A 2 by N_individual_parameters matrix of slopes. The first row gives effects of focal characteristics (on out-degree). 
 #' The second row gives effects of target characteristics (on in-degree).
-#' @param 
-#' dyadic_effects An N_dyadic_parameters vector of slopes.
+#' @param dyadic_effects An N_dyadic_parameters vector of slopes.
 #' @return A list of objects including: network (an adjacency matrix of binary outcomes), tie_strength (an adjacency matrix with probability weights), 
 #' group_ids (a vector of length N_id, giving the group of each individual), individual_predictors (the supplied covariate data is saved along with the network data), 
 #' and dyadic_predictors (the supplied covariate data is saved along with the network data).
@@ -83,10 +68,9 @@ simulate_sbm_plus_srm_network = function(N_id = 99,                            #
                                              dyadic_predictors = NULL,         # An array of covariates
                                              individual_effects = NULL,        # The effects of predictors on sender effects (row 1) and receiver effects (row 2)
                                              dyadic_effects = NULL             # The effects of predictors on dyadic ties
-                                         )
-{
-   ##################################### Run some checks
-   ######### Individual parameters
+                                         ){
+##################################### Run some checks
+######### Individual parameters
    if(!is.null(individual_predictors)){
    if(is.null(individual_effects)){
     stop("If individual_predictors is supplied, a matching matrix of individual_effects must be supplied.")
@@ -179,6 +163,3 @@ for ( i in 1:N_id ){
 
 return(list(network=y_true, tie_strength=p,  group_ids=groups, individual_predictors=individual_predictors, dyadic_predictors=dyadic_predictors, sr=sr, dr=dr, samps=samps))
 }
-
-
-
